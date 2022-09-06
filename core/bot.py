f16d3590e7b52b0f71d7cbaaf9261fb0bbce13b9
mc_fdc <Masato.04.11.2007@gmail.com>
2022-09-06 20:24:03 +0900

    Update bot.py

--- core/bot.py
+++ core/bot.py
@@ -1,5 +1,23 @@
 from discord.ext import commands
+import discord
+from orjson import loads
 
+from os import listdir
+
+from typing import TypedDict
+
+
+class Secret(TypedDict):
+    token: str
 
 class Develop(commands.Bot):
-    pass
+    with open("secret.json", "r") as f:
+        secret: Secret = loads(f.read())
+
+    async def setup_hook(self) -> None:
+        await self.load_extension("jishaku")
+        for filename in listdir("cogs"):
+            await self.load_extension(f"cogs.{filename[:-2]}")
+
+    async def on_ready(self) -> None:
+        print("Start")
